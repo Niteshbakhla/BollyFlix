eceb9bb78c5ef96ee81ec47a84d48e3063e3b7a7
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-11-25 11:37:50 +0530

    created a function to get the clicked movie details.

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Dashboard from './Dashboard'
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
+
 
 const MovieDetails = () => {
-  return (
-    <div>MovieDetails</div>
-  )
+            const { title } = useParams();
+            const decodedTitle = decodeURIComponent(title);
+            console.log(decodedTitle);
+            const getMoviesDetails = async () => {
+                        const res = await axios.get(import.meta.env.VITE_API_URL + `/get-movies/${decodedTitle}`)
+                        const { data } = res.data;
+                        console.log(data)
+            }
+            useEffect(() => {
+                        getMoviesDetails();
+            }, []);
+            return (
+                        <div>
+
+                        </div>
+            )
 }
 
 export default MovieDetails
