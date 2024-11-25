c4e1430be0048be5efbc9f798eaabf63667277ea
Nitesh bakhla <niteshbakhla007@gmail.com>
2024-11-25 11:37:14 +0530

    here also I used a navigate

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,13 +2,13 @@ import Allpost from '@/components/custom/Allpost';
 import Createpost from '@/components/custom/Createpost';
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
-            const navigate = useNavigate();
-            const tab = useSelector((state) => state.nav.tab)
+            const tab = useSelector((state) => state.nav.tab);
+     
             const renderComponent = () => {
                         switch (tab) {
+
                                     case "create-post":
                                                 return <Createpost />
                                     case "all-post":
